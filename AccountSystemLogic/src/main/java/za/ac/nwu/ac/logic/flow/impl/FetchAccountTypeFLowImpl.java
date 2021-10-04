47e4874efe13bdf22b7a9ff8b0c0911c157c65e8
Bianca2101 <blpiek21@gmail.com>
2021-10-04 20:51:43 +0200

    Spring Context

---
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/FetchAccountTypeFLowImpl.java
@@ -0,0 +1,20 @@
+package za.ac.nwu.ac.logic.flow.impl;
+
+import org.springframework.stereotype.Component;
+import za.ac.nwu.ac.logic.flow.FetchAccountTypeFLow;
+
+//@Transactional
+@Component
+public class FetchAccountTypeFLowImpl implements FetchAccountTypeFLow {
+    /*private final AccountTypeTranslator accountTypeTranslator;
+
+    @Autowired
+    public FetchAccountTypeFLowImpl(AccountTypeTranslator accountTypeTranslator){
+        this.AccountTypeTranslator = accountTypeTranslator;
+    }
+
+    @Override
+    public List<AccountTypeDto> getAllAccountTypes(){
+        return AccountTypeTranslator.getAllAccountTypes();
+    }*/
+}
