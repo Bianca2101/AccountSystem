ed85fd83ac98f8b64517f600f627f8d616b293be
Bianca2101 <blpiek21@gmail.com>
2021-10-05 22:13:46 +0200

    Data to DB

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -1,17 +1,19 @@
 package za.ac.nwu.ac.web.sb.controller;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import za.ac.nwu.ac.domain.dto.AccountTypeDto;
 import za.ac.nwu.ac.domain.service.GeneralResponse;
+import za.ac.nwu.ac.logic.flow.CreateAccountTypeFlow;
 import za.ac.nwu.ac.logic.flow.FetchAccountTypeFLow;
+
 import java.util.List;
 
 @RestController
@@ -19,10 +21,13 @@ import java.util.List;
 public class AccountTypeController {
 
     private final FetchAccountTypeFLow fetchAccountTypeFLow;
+    private final CreateAccountTypeFlow createAccountTypeFlow;
 
     @Autowired
-    public AccountTypeController(FetchAccountTypeFLow fetchAccountTypeFLow){
+    public AccountTypeController(FetchAccountTypeFLow fetchAccountTypeFLow,
+                                 @Qualifier("createAccountTypeFLowName") CreateAccountTypeFlow createAccountTypeFlow){
         this.fetchAccountTypeFLow = fetchAccountTypeFLow;
+        this.createAccountTypeFlow = createAccountTypeFlow;
     }
 
     @GetMapping("/all")
@@ -35,9 +40,24 @@ public class AccountTypeController {
     })
     public ResponseEntity<GeneralResponse<List<AccountTypeDto>>> getAll()
     {
-        List<AccountTypeDto> accountTypes = fetchAccountTypeFLow.getAllAccountTypes();         //>>insert accountTypes at payload if error
+        List<AccountTypeDto> accountTypes = fetchAccountTypeFLow.getAllAccountTypes();
         GeneralResponse<List<AccountTypeDto>> response = new GeneralResponse<>(true,accountTypes);
         return new ResponseEntity<>(response, HttpStatus.OK);
-
     }
+
+    @PostMapping("")
+    @ApiOperation(value = "Creates a new AccountType.", notes = "Creates a new AccountType in the DB.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "The AccountType was created successfully", response = GeneralResponse.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
+    public ResponseEntity<GeneralResponse<AccountTypeDto>> create(
+            @ApiParam(value = "Request body to create a new AccountType.",
+            required = true)
+            @RequestBody AccountTypeDto accountType){
+        AccountTypeDto accountTypeResponse = createAccountTypeFlow.create(accountType);
+        GeneralResponse<AccountTypeDto> response = new GeneralResponse<>(true, accountTypeResponse);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
 }
