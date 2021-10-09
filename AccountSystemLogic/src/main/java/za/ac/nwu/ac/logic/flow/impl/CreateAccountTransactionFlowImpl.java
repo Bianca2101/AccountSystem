871c2179bc64ae1c10a8730c693bb928cc8d65c0
Bianca2101 <blpiek21@gmail.com>
2021-10-09 10:25:45 +0200

    Logging for create account transaction flow

--- AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/CreateAccountTransactionFlowImpl.java
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/CreateAccountTransactionFlowImpl.java
@@ -1,5 +1,7 @@
 package za.ac.nwu.ac.logic.flow.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import za.ac.nwu.ac.domain.dto.AccountTransactionDto;
 import za.ac.nwu.ac.domain.dto.AccountTypeDto;
@@ -10,10 +12,13 @@ import za.ac.nwu.ac.logic.flow.FetchAccountTypeFLow;
 import za.ac.nwu.ac.translator.AccountTransactionDetailsTranslator;
 import za.ac.nwu.ac.translator.AccountTransactionTranslator;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @Component
 public class CreateAccountTransactionFlowImpl implements CreateAccountTransactionFlow {
 
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreateAccountTransactionFlowImpl.class);
 
     private final AccountTransactionTranslator accountTransactionTranslator;
     private final AccountTransactionDetailsTranslator accountTransactionDetailsTranslator;
@@ -27,9 +32,22 @@ public class CreateAccountTransactionFlowImpl implements CreateAccountTransactio
 
     @Override
     public AccountTransactionDto create(AccountTransactionDto accountTransactionDto){
+        if (LOGGER.isInfoEnabled()){
+            String outputForLogging = null;
+            if((null != accountTransactionDto) && (null != accountTransactionDto.getDetails())){
+                outputForLogging = accountTransactionDto.getDetails().toString();
+
+            }
+            LOGGER.info("The input object is {} and the Details is {}", accountTransactionDto, outputForLogging);
+        }
         accountTransactionDto.setTransactionID(null);
 
         AccountTypeDto accountType = fetchAccountTypeFLow.getAccountTypeByMnemonic(accountTransactionDto.getAccountTypeMnemonic());
+
+        if(LOGGER.isDebugEnabled()){
+            LOGGER.debug("Got AccountType for {} and the AccountTypeID is {}", accountTransactionDto.getAccountTypeMnemonic(),accountType.getAccountTypeId());
+        }
+
         AccountTransaction accountTransaction = accountTransactionDto.buildAccountTransaction(accountType);
 
         AccountTransaction createdAccountTransaction = accountTransactionTranslator.save(accountTransaction);
@@ -39,6 +57,8 @@ public class CreateAccountTransactionFlowImpl implements CreateAccountTransactio
             createdAccountTransaction.setDetails(accountTransactionDetails);
             accountTransactionDetailsTranslator.save(createdAccountTransaction.getDetails());
         }
-        return new AccountTransactionDto(createdAccountTransaction);
+        AccountTransactionDto results = new AccountTransactionDto(createdAccountTransaction);
+        LOGGER.info("The return object is {}", results);
+        return results;
     }
 }
